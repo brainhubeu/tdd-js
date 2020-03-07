ab8c72e160ec3a330c5a35f28dee8d648480474d
piotr-s-brainhub <piotr.sroczkowski@brainhub.pl>
2020-03-08 00:54:00 +0100

    mocha tests

---
+++ tests/splitStringIntoChunks.js
@@ -0,0 +1,17 @@
+const splitStringIntoChunks = require('../splitStringIntoChunks');
+const { expect } = require('chai');
+
+describe('splitStringIntoChunks', () => {
+  it('works', () => {
+    expect(splitStringIntoChunks('', 2)).to.deep.equal([]);
+    expect(splitStringIntoChunks('a', 2)).to.deep.equal(['a']);
+    expect(splitStringIntoChunks('ab', 2)).to.deep.equal(['ab']);
+    expect(splitStringIntoChunks('abc', 2)).to.deep.equal(['ab', 'c']);
+    expect(splitStringIntoChunks('abcd', 2)).to.deep.equal(['ab', 'cd']);
+    expect(splitStringIntoChunks('abcde', 2)).to.deep.equal(['ab', 'cd', 'e']);
+    expect(splitStringIntoChunks('abcde', 3)).to.deep.equal(['abc', 'de']);
+    expect(splitStringIntoChunks('abcde', 4)).to.deep.equal(['abcd', 'e']);
+    expect(splitStringIntoChunks('abcde', 5)).to.deep.equal(['abcde']);
+    expect(splitStringIntoChunks('abcde', 6)).to.deep.equal(['abcde']);
+  });
+});
