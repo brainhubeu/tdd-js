e54237e49423ecbed5ba3c662192c07a06f8a08a
piotr-s-brainhub <piotr.sroczkowski@brainhub.pl>
2020-03-11 00:10:01 +0100

    show nyc bug

--- tests/isLeapYear.js
+++ tests/isLeapYear.js
@@ -8,22 +8,4 @@ describe('isLeapYear', () => {
 
     expect(result).to.equal(false);
   });
-
-  it('returns true for a year divisible by 4', () => {
-    const result = isLeapYear(2020);
-
-    expect(result).to.equal(true);
-  });
-
-  it('returns false for a year divisible by 100', () => {
-    const result = isLeapYear(2100);
-
-    expect(result).to.equal(false);
-  });
-
-  it('returns true for a year divisible by 400', () => {
-    const result = isLeapYear(2000);
-
-    expect(result).to.equal(true);
-  });
 });
