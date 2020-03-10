fdda0962df666316a1f98b7934dc4b9838d12e3e
piotr-s-brainhub <piotr.sroczkowski@brainhub.pl>
2020-03-11 00:13:03 +0100

    show nyc correct behavior

--- tests/isLeapYear.js
+++ tests/isLeapYear.js
@@ -1,29 +1,5 @@
-const { expect } = require('chai');
-
+// eslint-disable-next-line no-unused-vars
 const isLeapYear = require('../isLeapYear');
 
 describe('isLeapYear', () => {
-  it('returns false for a year non-divisible by 4', () => {
-    const result = isLeapYear(2019);
-
-    expect(result).to.equal(false);
-  });
-
-  it('returns true for a year divisible by 4', () => {
-    const result = isLeapYear(2020);
-
-    expect(result).to.equal(true);
-  });
-
-  it('returns false for a year divisible by 100', () => {
-    const result = isLeapYear(2100);
-
-    expect(result).to.equal(false);
-  });
-
-  it('returns true for a year divisible by 400', () => {
-    const result = isLeapYear(2000);
-
-    expect(result).to.equal(true);
-  });
 });
